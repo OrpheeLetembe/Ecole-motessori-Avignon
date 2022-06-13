41503fe761ca243cebf607a36f34c35938f9b933
Orphée <letembeorphee@gmail.com>
2022-06-13 02:30:09 +0200

    add from to three_six

--- suivi/three_six/forms.py
+++ suivi/three_six/forms.py
@@ -49,4 +49,32 @@ class PracticalLifeForm(forms.Form):
                                          choices=LEVEL_CHOICES, widget=forms.RadioSelect())
     open_close_boxes = forms.ChoiceField(label="Ouvrir et fermer des boites",
                                          choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    open_close_bottles = forms.ChoiceField(label="Ouvrir et fermer des flacons",
+                                           choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    fold_fabrics = forms.ChoiceField(label="Plier des étoffes", choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    fold_paper = forms.ChoiceField(label="Plier du papier", choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    cut_paper_scissors = forms.ChoiceField(label="Couper du papier avec des ciseaux",
+                                          choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    paste_paper = forms.ChoiceField(label="Coller du papier", choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    dust = forms.ChoiceField(label="Epousseter avec un chiffon, un plumeau",
+                             choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    sweep = forms.ChoiceField(label="Balayer", choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    brush_carpet = forms.ChoiceField(label="Brosser un tapis", choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    transfer_spoon = forms.ChoiceField(label="Transvaser avec une cuillère",
+                                       choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    pour_rice = forms.ChoiceField(label="Verser du riz", choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    pour_sand = forms.ChoiceField(label="Verser du sable", choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    pour_water = forms.ChoiceField(label="Verser de l'eau", choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    pour_water_glasses = forms.ChoiceField(label="Verser de l'eau dans des verres",
+                                           choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    clean_mirror = forms.ChoiceField(label="Nettoyer un miroir", choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    polish_brass = forms.ChoiceField(label="Astiquer les cuivres", choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    take_care_plants = forms.ChoiceField(label="Soigner les plantes", choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    chang_flower_water = forms.ChoiceField(label="Changer l'eau des fleurs",
+                                           choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    wash_table = forms.ChoiceField(label="Laver la table", choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    wash_clothes = forms.ChoiceField(label="Laver du linge", choices=LEVEL_CHOICES, widget=forms.RadioSelect())
+    observations_care_interior = forms.CharField(label="Observations soin du milieu intérieur ",
+                                                         widget=forms.Textarea(attrs={"rows": 5, "cols": 81}))
+
 
