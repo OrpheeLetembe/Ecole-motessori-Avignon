660dcf7330914a025e57477e35da54ea750a5f84
Orphée <letembeorphee@gmail.com>
2022-07-03 21:35:04 +0200

    add change procfile

--- suivi/authentication/forms.py
+++ suivi/authentication/forms.py
@@ -46,31 +46,3 @@ class SignUpForm(UserCreationForm):
         if commit:
             user.save()
         return user
-
-"""
-class SignupForm(UserCreationForm):
-
-    CHOICES = (
-        ('educator', 'Educatrice'),
-        ('assistant', 'Assistante')
-    )
-
-    username = forms.CharField(required=True, label="Nom d'utilisateur",
-                               widget=forms.TextInput(attrs={"class": "form-control"}))
-    first_name = forms.CharField(label="Prénom", widget=forms.TextInput(attrs={"class": "form-control"}))
-    last_name = forms.CharField(label="Nom", widget=forms.TextInput(attrs={"class": "form-control"}))
-    role = forms.ChoiceField(
-        label="Rôle", required=False,
-        choices=CHOICES, widget=forms.Select(attrs={"class": "form-control"}))
-    ambiance = forms.ModelChoiceField(
-        label="Ambiance", required=False, queryset=Ambiance.objects.all(), empty_label="-----",
-        widget=forms.Select(attrs={'class': 'form-control'}))
-    password1 = forms.CharField(label="Mot de passe", widget=forms.PasswordInput(attrs={"class": "form-control"}))
-    password2 = forms.CharField(label="Confirmer mot de passe",
-                                widget=forms.PasswordInput(attrs={"class": "form-control"}))
-
-    class Meta(UserCreationForm.Meta):
-        model = get_user_model()
-
-"""
-
